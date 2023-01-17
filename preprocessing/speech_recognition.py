0a9ce3dbbac3a321cb7463d41d6715204d17a751
Balazs G. Horvath <bghorvath@pm.me>
2023-01-17 11:21:26 +0100

    time for notification

--- preprocessing/speech_recognition.py
+++ preprocessing/speech_recognition.py
@@ -1,4 +1,5 @@
 import os
+import time
 import json
 import whisper
 from preprocessing.notify import notify
@@ -27,8 +28,10 @@ if __name__ == "__main__":
     speech_recognition = SpeechRecognition("large-v2")
     audio_list = [f for f in os.listdir(audio_dir) if f.endswith(".mp3") and not os.path.exists(os.path.join(text_dir, f"{f[:-4]}_transcript.json"))]
     for i, f in enumerate(audio_list):
+        start = time.time()
         audio_path = os.path.join(audio_dir, f)
         result = speech_recognition.transcribe(audio_path)
-        notify(f"Job {i+1}/{len(audio_list)} done: transcript for {f}")
         with open(os.path.join(text_dir, f"{f[:-4]}_transcript.json"), "w") as f:
             json.dump(result, f, indent=4)
+        end = time.time()
+        notify(f"Job {i+1}/{len(audio_list)} done in {(end-start)//3600}h {(end-start)//60}m: transcribed {f}")
