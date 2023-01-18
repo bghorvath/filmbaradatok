1425e882eb28da0018ce33e15870f3891918b5cf
Balazs G. Horvath <bghorvath@pm.me>
2023-01-18 10:10:59 +0100

    add eta to notification

--- preprocessing/speech_recognition.py
+++ preprocessing/speech_recognition.py
@@ -31,19 +31,22 @@ if __name__ == "__main__":
     text_dir = "data/speech_recognition"
     speech_recognition = SpeechRecognition("large-v2")
     audio_list = []
+    audio_length_dict = {}
     for audio_file in os.listdir(audio_dir):
         audio_length = ffmpeg.probe(os.path.join(audio_dir, audio_file)).get("format", {}).get("duration")
+        audio_length_dict[audio_file] = audio_length
         if audio_file.endswith(".mp3") and not os.path.exists(os.path.join(text_dir, f"{audio_file[:-4]}.json")) and float(audio_length) <  16000:
             audio_list.append(audio_file)
     
     for i, audio_file in enumerate(audio_list):
         gc.collect()
-        current_time = time.strftime("%H:%M:%S", time.localtime())
-        print(f"{current_time} |  starting job {i+1}/{len(audio_list)}: transcribing {audio_file}")
+        current_time = time.strftime("%H:%M", time.localtime())
+        eta = time.strftime("%H:%M", time.localtime(time.time() + audio_length_dict[audio_file]/2))
+        notify(f"{current_time} | STARTED transcribing {audio_file} | {i+1}/{len(audio_list)} | ETA: {eta}")
         start = time.time()
-        audio_path = os.path.join(audio_dir, audio_file)
-        result = speech_recognition.transcribe(audio_path)
+        result = speech_recognition.transcribe(os.path.join(audio_dir, audio_file))
         end = time.time()
-        notify(f"Job {i+1}/{len(audio_list)} done in {int((end-start)//3600)}h {int(((end-start)%3600)//60)}m: transcribed {audio_file}")
+        current_time = time.strftime("%H:%M", time.localtime())
+        notify(f"{current_time} | FINISHED transcribing {audio_file} | {i+1}/{len(audio_list)} | TOOK {int((end-start)//3600)}h {int(((end-start)%3600)//60)}m")
         with open(os.path.join(text_dir, f"{audio_file[:-4]}.json"), "w") as f:
             json.dump(result, f, indent=4)
